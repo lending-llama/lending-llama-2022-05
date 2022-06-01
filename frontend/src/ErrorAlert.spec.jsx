f2007c99b7d6d2dca1a3a5166ad142bd3ce59bf1
Alex Bepple <alex@bepple.de>
2022-06-01 12:36:12 +0200

    . isolate tests

--- frontend/src/ErrorAlert.spec.jsx
+++ frontend/src/ErrorAlert.spec.jsx
@@ -2,18 +2,19 @@ import {ErrorAlert} from "./ErrorAlert";
 import {render} from "@testing-library/react";
 import React from "react";
 import {Provider} from "react-redux";
-import {store} from "./redux";
+import {createMyStore, store} from "./redux";
 import '@testing-library/jest-dom'
 import {errorsAdded, errorsDismissedFirst} from "./actions/errors"; // extends Jest with .toHaveTextContent
 
 describe('Error Alert', () => {
   it('displays error msg', () => {
-    const alert = render(<Provider store={store}><ErrorAlert/></Provider>);
+    const alert = render(<Provider store={createMyStore()}><ErrorAlert/></Provider>);
     store.dispatch(errorsAdded('fdsa'))
     expect(alert.queryAllByText('fdsa')).toHaveLength(1)
   })
   it('dismisses error msg', async () => {
-    const alert = render(<Provider store={store}><ErrorAlert/></Provider>);
+    const alert = render(<Provider store={createMyStore()}><ErrorAlert/></Provider>);
+    store.dispatch(errorsAdded('fdsa'))
     store.dispatch(errorsDismissedFirst())
     expect(alert.queryAllByText('fdsa')).toEqual([])
   })
--- frontend/src/redux.js
+++ frontend/src/redux.js
@@ -3,11 +3,15 @@ import {errorsReducer} from "./reducers/errors";
 import {featuresReducer} from "./reducers/features";
 import {allocationsReducer} from "./reducers/allocations";
 
-export const store = createStore(
-  combineReducers({
-    allocations: allocationsReducer,
-    features: featuresReducer,
-    errors: errorsReducer,
-  }),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+export function createMyStore() {
+  return createStore(
+    combineReducers({
+      allocations: allocationsReducer,
+      features: featuresReducer,
+      errors: errorsReducer,
+    }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+}
+
+export const store = createMyStore()
