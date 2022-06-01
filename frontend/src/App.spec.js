d7650cd6081ce55dd78fc242b30951c50af7d23f
Alex Bepple <alex@bepple.de>
2022-06-01 14:19:12 +0200

    . remove obsolete test

--- frontend/src/App.spec.js
+++ frontend/src/App.spec.js
@@ -6,7 +6,6 @@ import {Provider} from "react-redux";
 import {store} from "./redux";
 import 'whatwg-fetch' // sets global.fetch
 import '@testing-library/jest-dom'
-import {formatAllocationRate} from "./reducers/allocations"; // extends Jest with .toHaveTextContent
 
 describe('Llending Llama UI', () => {
   beforeEach(nock.cleanAll)
@@ -23,16 +22,4 @@ describe('Llending Llama UI', () => {
     expect(c.getByTestId('allocation-c020b901')).toHaveTextContent('7.00%') // .00 because of other test
   })
 
-  it('works with 3 decimal places', async () => {
-    nock(/./)
-      .get('/api/best-rate')
-      .reply(200, {name: 'foo', rate: 7.168})
-    nock(/./).get(x => x.includes('/allocation')).reply(200, [])
-
-    const c = render(<Provider store={store}><App/></Provider>);
-    await waitFor(() => c.getByText('foo', {exact:false}))
-    await waitFor(() => c.getByText('7.17', {exact:false}))
-    await new Promise((resolve) => setTimeout(resolve, 10));
-    expect(c.getByTestId('allocation-c020b901')).toHaveTextContent('7.17%')
-  })
 })
