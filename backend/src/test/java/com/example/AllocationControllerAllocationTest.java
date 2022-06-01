f64ffc0a4ee2de9878ec29f5346e791a21e0c2ad
Tobias Beckmann <beckmann@sipgate.de>
2022-06-01 15:26:05 +0200

    remove depedency on external server from test

--- backend/src/test/java/com/example/AllocationControllerAllocationTest.java
+++ backend/src/test/java/com/example/AllocationControllerAllocationTest.java
@@ -1,35 +1,65 @@
 package com.example;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
+import java.net.URI;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class AllocationControllerAllocationTest {
+class AllocationControllerAllocationTest {
 
 	@Autowired
 	private MockMvc mvc;
+    @Autowired
+    private RestTemplate restTemplate;
 
-    private ObjectMapper mapper = new ObjectMapper();
+    private MockRestServiceServer mockServer;
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @BeforeEach
+    public void beforeEach() {
+        mockServer = MockRestServiceServer.createServer(restTemplate);
+    }
 
     @Test
-	public void findsBest() throws Exception {
-        var expected = Arrays.asList(
-            new Allocation().setName("Ledn").setRate(6.25),
-            new Allocation().setName("BlockFi").setRate(4.5)
+    void selectsAllocationsFromAvailable() throws Exception {
+        var url = "https://priceless-khorana-4dd263.netlify.app/btc-rates.json";
+
+        final var platformName = "platform1";
+        final var platformRate = 5.0;
+
+        var expected = List.of(
+            new Allocation().setName(platformName).setRate(platformRate)
         );
+
+        var platformFromExternalApi = new Platform()
+            .setName(platformName)
+            .setTiers(new Platform.Tier[]{new Platform.Tier().setRate(platformRate)});
+
+        mockServer.expect(requestTo(new URI(url)))
+            .andRespond(withStatus(HttpStatus.OK)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(mapper.writeValueAsString(List.of(platformFromExternalApi))));
+
         mvc.perform(MockMvcRequestBuilders.get("/allocations?amount=1.1")
             .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
@@ -37,7 +67,7 @@ public class AllocationControllerAllocationTest {
 	}
 
     @Test
-    public void testReplacesMissingMaxInPlatformTierWithInfinity(){
+    void testReplacesMissingMaxInPlatformTierWithInfinity(){
 
         final Platform.Tier[] tiers = new Platform.Tier[1];
         tiers[0] = new Platform.Tier().setRate(1.0); // set a rate but no Max value!
