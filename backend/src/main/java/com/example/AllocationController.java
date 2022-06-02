dca8feee0426c27921108eb34da46741cb3f91f1
Tobias Beckmann <beckmann@sipgate.de>
2022-06-02 11:21:17 +0200

    feat: make "best-rate"-endpoint flexible, e.g. "best-rate-eth"
    Co-authored-by: Julius Flohr <flohr@sipgate.de>

--- backend/src/main/java/com/example/AllocationController.java
+++ backend/src/main/java/com/example/AllocationController.java
@@ -3,6 +3,7 @@ package com.example;
 import io.split.client.SplitClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,8 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -28,9 +31,24 @@ public class AllocationController {
         this.splitClient = splitClient;
     }
 
-    @GetMapping("/best-rate")
-    public Allocation getBestRate() {
-        var tier1 = getPlatformTiersDescByRate("btc-rates.json").get(0);
+    @GetMapping(value={"/best-rate", "/best-rate-{currencyParam}"})
+    public Allocation getBestRate(@PathVariable Optional<String> currencyParam) {
+        var apiLocations = Map.of("btc", "btc-rates.json",
+        "eth", "eth-rates");
+        var currencyName = currencyParam.orElseGet(()->"btc");
+        if (!apiLocations.containsKey(currencyName)){
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Foo Not Found");
+        }
+
+        var apiLocation = apiLocations.get(currencyName);
+
+        var tier1 = getPlatformTiersDescByRate(apiLocation).get(0);
+        return new Allocation().setName(tier1.getName()).setRate(tier1.getRate());
+    }
+
+    public Allocation getBestEthRate() {
+        var tier1 = getPlatformTiersDescByRate("eth-rates.json").get(0);
         return new Allocation().setName(tier1.getName()).setRate(tier1.getRate());
     }
 
@@ -68,8 +86,5 @@ public class AllocationController {
                 .setMax( null == t.getMax()? Double.POSITIVE_INFINITY : t.getMax()));
     }
 
-    public Allocation getBestEthRate() {
-        var tier1 = getPlatformTiersDescByRate("eth-rates.json").get(0);
-        return new Allocation().setName(tier1.getName()).setRate(tier1.getRate());
-    }
+
 }
--- backend/src/test/java/com/example/AllocationControllerBestRateTest.java
+++ backend/src/test/java/com/example/AllocationControllerBestRateTest.java
@@ -1,9 +1,12 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,9 +15,17 @@ public class AllocationControllerBestRateTest {
     @Autowired
     private TestRestTemplate template;
 
-    @Test
-    public void returnsBestRate() throws Exception {
-        var res = template.getForEntity("/best-rate", Allocation.class);
+    @ParameterizedTest
+    @ValueSource(strings = {"/best-rate", "/best-rate-btc"})
+    public void returnsBestRate(String url) throws Exception {
+        var res = template.getForEntity(url, Allocation.class);
         assertThat(res.getBody()).isEqualTo(new Allocation().setName("Ledn").setRate(6.25));
+    }
+
+    @Test
+    void returns404ForUnknownCoins(){
+        var notACoin = "/best-rate-asdf";
+        var res = template.getForEntity(notACoin, Allocation.class);
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 }
