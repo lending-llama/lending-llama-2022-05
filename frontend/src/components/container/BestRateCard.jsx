52f013d25b593f63de18826d64a0399434a8874b
Luca Schmitz <schmitz@sipgate.de>
2022-06-02 11:24:02 +0200

    extract and relocate fetch

--- frontend/src/components/container/BestRateCard.jsx
+++ frontend/src/components/container/BestRateCard.jsx
@@ -1,26 +1,25 @@
-import {Card, formatAllocationRate} from "../presentation";
-import React, {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {bestRateFetched} from "../../redux/actions/allocations";
+import { Card, formatAllocationRate } from "../presentation";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { bestRateFetched } from "../../redux/actions/allocations";
+import {myFetch} from "../../utils/format";
 
 export function BestRateCard() {
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
-
-  const bestAllocation = useSelector(x => x.allocations.bestRate)
+  const bestAllocation = useSelector((x) => x.allocations.bestRate);
   useEffect(() => {
-    fetch(`/api/best-rate`)
-      .then(x => x.json())
-      .then(x => dispatch(bestRateFetched(x)))
-  }, [])
-
+    myFetch(`best-rate`, dispatch, bestRateFetched);
+  }, []);
 
   return (
     <div data-testid="allocation-c020b901">
       <Card>
-        Best rate: {formatAllocationRate(bestAllocation.rate)} ({bestAllocation.name})
+        Best rate: {formatAllocationRate(bestAllocation.rate)} (
+        {bestAllocation.name})
       </Card>
     </div>
-  )
-
+  );
 }
+
+
--- frontend/src/utils/format.js
+++ frontend/src/utils/format.js
@@ -2,3 +2,17 @@
 export const formatAllocationRate = (rate) => {
   return rate && rate.toFixed(2) + "%";
 }
+
+export const myFetch = (url, dispatch, action) => {
+  const baseUrl = '/api/';
+  fetch(baseUrl + url)
+    .then(async (x) => {
+      if (x.status >= 400) {
+        throw new Error(await x.text());
+      }
+      return x;
+    })
+    .then((x) => x.json())
+    .then((x) => dispatch(action(x)))
+    .catch((e) => dispatch(errorsAdded(e.message)));
+};
